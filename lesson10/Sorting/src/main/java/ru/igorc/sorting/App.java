8e453e92fc1f4392eff65486f4c210c0d350eca3
Igor Chernyshov <26788114+chernyshov-ia@users.noreply.github.com>
2022-02-10 09:15:25 +0300

    lesson 10

---
+++ lesson10/Sorting/src/main/java/ru/igorc/sorting/App.java
@@ -0,0 +1,56 @@
+package ru.igorc.sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Consumer;
+
+public class App {
+    public final static int SIZE = 50000;
+
+    public static List<Integer> generateList(int size) {
+        List<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(ThreadLocalRandom.current().nextInt(size));
+        }
+        return list;
+    }
+
+    /**
+     * Тупая сортировка выбором
+     */
+    public static void sort_selection(List<Integer> list) {
+        if (list.size() < 2) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
+    }
+
+    public static void sortTest(List<Integer> list, Consumer<List<Integer>> func, String hello) {
+        List<Integer> testList = new ArrayList<>(list);
+        System.out.println("--------------------------------------------------");
+        if (hello != null) {
+            System.out.println(hello);
+        }
+        Date now = new Date();
+        func.accept(testList);
+        long time = new Date().getTime() - now.getTime();
+        System.out.println("Затраченное время на сортировку: " + time + " ms");
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = generateList(SIZE);
+        sortTest(list, App::sort_selection, "Сортировка выбором");
+        sortTest(list, Collections::sort, "Сортировка Collections.sort");
+    }
+}
